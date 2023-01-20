ac73148ed043b7a687a514fce18ff5d20a38470d
josedavinup <josedavidnup@gmail.com>
2023-01-20 16:35:55 -0400

    installing fastAPI

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,29 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+
+@app.get('/')
+def get_list():
+    return [
+        1, 2, 3, 4
+    ]
+
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_contact():
+    return """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Hola Bolivia</h1>
+        </body>
+    </html>
+    """
 
 
 def run():
